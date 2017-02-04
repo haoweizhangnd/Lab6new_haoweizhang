2f68e922d6dc36eeabb64b689107a1b1b543dba6
haoweizhangnd <hzhang8@nd.edu>
2017-02-04 22:38:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/alienware/lab_haoweizhang/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.alienware.lab_haoweizhang;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Alienware on 2017/2/3.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
